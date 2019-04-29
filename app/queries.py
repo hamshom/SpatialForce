1e01451e0e3c53406acf82f69dabcd0d6ed8d73e
tpiplani <tanyapiplani.16@gmail.com>
2019-04-29 16:42:15 -0700

    modified queries

--- app/queries.py
+++ app/queries.py
@@ -2,15 +2,26 @@ import database as db
 
 
 def get_users():
-    query = "SELECT * FROM spatialforce.GEO"
-    return db.query_db(query)
+    query = "SELECT * FROM spatialforce.Geo"
+    conn = db.connect()
+    cursor = conn.cursor(buffered = True)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
 
 
 def get_data():
     conn = db.connect()
-    cursor = conn.cursor()
+    cursor = conn.cursor(buffered = True)
     sql = "SELECT * FROM spatialforce.Geo"
     cursor.execute(sql)
-    result = cursor.fetchone()
+    result = cursor.fetchmany()
+    return result
 
+def get_geo(state, county, tract):
+    sql = "Select * FROM spatialforce.Geo where state = '%d' and county = '%d' and tract = '%d'" % (state, county, tract)
+    conn = db.connect()
+    cursor = conn.cursor(buffered = True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
     return result
