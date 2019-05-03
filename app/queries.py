37779d72e465263ca856c83b57cc891523777b89
tpiplani <tanyapiplani.16@gmail.com>
2019-05-02 18:11:42 -0700

    new queries

--- app/queries.py
+++ app/queries.py
@@ -25,3 +25,19 @@ def get_geo(state, county, tract):
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
+
+def get_race_data_byzip(zip_code):
+    sql = "select * from race_2010 inner join zipcode_to_geoid on race_2010.state_id = zipcode_to_geoid.state_id and race_2010.county_id = zipcode_to_geoid.county_id and race_2010.tract_id = zipcode_to_geoid.tract_id and zipcode_to_geoid.zip_code = %s" % (zip_code)
+    conn = db.connect()
+    cursor = conn.cursor(buffered = True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+def get_top_population():
+    sql = "SELECT * FROM race_2010 order by total_pop desc limit 10"
+    conn = db.connect()
+    cursor = conn.cursor(buffered = True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
